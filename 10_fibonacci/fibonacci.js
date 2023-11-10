f19809f50a463be90c8a99bd8a755136071b2a6e
Kev <87592672+0xkev21@users.noreply.github.com>
2023-11-10 23:51:46 +0630

    Finish 10 fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    if (index === 0) return 0;
+    let first = 0,
+    second = 1;
+    for (let i = 2; i <= index; i++) {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+    return second;
 };
 
 // Do not edit below this line
