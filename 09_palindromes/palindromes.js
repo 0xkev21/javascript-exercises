e96e11ee8bb38e1898c12eb4af8fed39a2799123
Kev <87592672+0xkev21@users.noreply.github.com>
2023-11-10 23:00:56 +0630

    Finish 09 palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.split('').filter(l => (/[a-zA-Z0-9]+/).test(l)).join('').toLowerCase();
+    return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
