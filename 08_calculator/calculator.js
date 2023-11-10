5313e17b719f168afc6d874d68e168a310f41021
Kev <87592672+0xkev21@users.noreply.github.com>
2023-11-10 22:28:38 +0630

    Finish 08 calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((a, b) => a + b, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let f = 1;
+	for(let i = a; i > 0; i--) {
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
