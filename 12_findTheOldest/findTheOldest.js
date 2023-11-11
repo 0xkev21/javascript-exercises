1ced3c43d0e8ab1bdb082e393b5efb1e14259da6
Kev <87592672+0xkev21@users.noreply.github.com>
2023-11-11 16:13:13 +0630

    Finish 12 findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestAge = 0;
+    return arr.reduce((acc, item) => {
+        const age = (item.yearOfDeath || (new Date).getFullYear()) - item.yearOfBirth;
+        if(age > oldestAge) {
+            oldestAge = age;
+            return item;
+        }
+        return acc;
+    }, {})
 };
 
 // Do not edit below this line
